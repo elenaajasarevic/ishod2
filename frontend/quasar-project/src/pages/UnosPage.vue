f79179e81c220b8ba7c6fbbd4bba4227561c9bf9
elenaajasarevic <elenaa.jasarevic@gmail.com>
2025-11-27 13:22:12 +0100

    Dodana mapa

---
+++ frontend/quasar-project/src/pages/UnosPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn color="white" text-color="black" label="Standard" />
+  </div>
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
